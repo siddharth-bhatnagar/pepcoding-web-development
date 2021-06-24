7c6f091fa5ebe8aa708fb11cf8597a41c74b7e59
siddharth-bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-06-24 18:24:44 +0530

    Init

---
+++ movies/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './styles/App.css';
+import Movies from './components/Movies/Movies';
+
+function App() {
+    return (
+        <div>
+            <Movies />
+        </div>
+    )
+}
+
+export default App;
